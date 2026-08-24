c0fa3ae46a31a8f4313397885c122efdbb273d49
dinhngoctuyen4125 <tuyenotk4125@gmail>
2026-08-24 16:18:06 +0700

    lmao

--- test_model_utility.sh
+++ test_model_utility.sh
@@ -36,7 +36,7 @@ export LD_LIBRARY_PATH=/home/ritsu/miniconda3/envs/simnpo/lib/python3.10/site-pa
 
 /home/ritsu/miniconda3/envs/prod_eval/bin/python evaluatre.py \
     --dataset HumanEval \
-    --input_path "${OUTPUT_DIR}/HumanEval_SimNPO-CodeLlama-7b_temp0.2_toppNone_topkNone_samples5_0shot_${SUFFIX}.jsonl" \
+    --input_path "HumanEval_CodeLlama-7b-hf_temp0.2_toppNone_topkNone_samples5_0shot_2026.jsonl" \
     --truncate \
     --eval_standard \
     --k_list 1 3 5
